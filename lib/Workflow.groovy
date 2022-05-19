1e00c597f1d2983db3f95c1745335bc61eb55d28
njspix <56930974+njspix@users.noreply.github.com>
2022-05-19 16:22:09 -0400

    add fasta param back in

--- lib/Workflow.groovy
+++ lib/Workflow.groovy
@@ -35,10 +35,10 @@ class Workflow {
     static void validateWorkflowParams(params, log) {
         genomeExists(params, log)
 
-        // if (!params.fasta) {
-        //     log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-        //     System.exit(1)
-        // }
+        if (!params.fasta) {
+            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
+            System.exit(1)
+        }
     }
 
     // Exit pipeline if incorrect --genome key provided
