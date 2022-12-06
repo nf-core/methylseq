0e105ef38f3ac039ba2c3d529c5dbda5a360fd2e
Phil Ewels <phil.ewels@seqera.io>
2022-12-06 15:34:26 +0000

    Fix changes in the sample sheet validation script

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -196,13 +196,13 @@ def check_samplesheet(file_in, file_out):
     # See https://docs.python.org/3.9/library/csv.html#id3 to read up on `newline=""`.
     with file_in.open(newline="") as in_handle:
         dialect = sniff_format(in_handle)
-        logger.info(f"Detected samplesheet format: {dialect}")
+        logger.info(f"Detected samplesheet format: '{dialect.delimiter}'")
         reader = csv.DictReader(in_handle, dialect=dialect)
         # Validate the existence of the expected header columns.
         if not required_columns.issubset(reader.fieldnames):
             req_cols = ", ".join(required_columns)
-            logger.critical(f"The sample sheet **must** contain these column headers: {req_cols}.")
-            logger.info(f"Found the following: {', '.join(reader.fieldnames)}")
+            logger.critical(f"The sample sheet **must** contain these column headers: {req_cols}")
+            logger.critical(f"Found the following: {', '.join(reader.fieldnames)}")
             sys.exit(1)
         # Validate each row.
         checker = RowChecker()
@@ -247,7 +247,7 @@ def parse_args(argv=None):
         "--log-level",
         help="The desired log level (default WARNING).",
         choices=("CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"),
-        default="WARNING",
+        default="INFO",
     )
     return parser.parse_args(argv)
 
