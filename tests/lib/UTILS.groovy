428ae98965edacb38831f8066d36b67d6008a422
Sateesh <perisateesh@gmail.com>
2024-09-19 15:22:35 +0000

    use nft-bam plugin - init default

--- tests/lib/UTILS.groovy
+++ tests/lib/UTILS.groovy
@@ -1,7 +1,7 @@
 // Function to remove Nextflow version from software_versions.yml
 
 class UTILS {
-    public static String removeNextflowVersion(outputDir) {
+    public static Object removeNextflowVersion(outputDir) {
         def softwareVersions = path("$outputDir/pipeline_info/nf_core_pipeline_software_mqc_versions.yml").yaml
         if (softwareVersions.containsKey("Workflow")) {
             softwareVersions.Workflow.remove("Nextflow")
